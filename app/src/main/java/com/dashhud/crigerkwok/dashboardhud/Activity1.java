836a853b82c70d3a3ceaab73b214fcaae1c04c3e
dcriger <dcriger@mail.csuchico.edu>
2018-01-23 11:52:24 -0800

    First Upload  - Google maps connect button with no functionality  - Status string that will change later with connection status  - Select FM Radio Station section works with slider and left/right buttons to display a set radio station.  - No shared preferences yet, does not save states.  - No bluetooth functionality yet

---
+++ app/src/main/java/com/dashhud/crigerkwok/dashboardhud/Activity1.java
@@ -0,0 +1,82 @@
+package com.dashhud.crigerkwok.dashboardhud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class Activity1 extends AppCompatActivity {
+
+    TextView gmaps_status;
+    TextView fm_current;
+
+    Button connect_gmaps;
+
+    ImageButton fm_back;
+    ImageButton fm_forward;
+
+    SeekBar fm_select;
+
+    String connected = "Connected";
+    String not_connected = "Not Connected";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+        gmaps_status = findViewById(R.id.maps_connected_text);
+        fm_current = findViewById(R.id.FM_frequency_text);
+
+        connect_gmaps = findViewById(R.id.connect_google_maps);
+
+        fm_back = findViewById(R.id.FM_prev);
+        fm_forward = findViewById(R.id.FM_next);
+
+        fm_select = findViewById(R.id.FM_radio_select);
+
+        update_station();
+
+    }
+
+    public void update_station()
+    {
+        fm_select.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar sb, int progress, boolean from_user) {
+                Double sb_current = progress * 0.2;
+                sb_current = sb_current + (Double) 88.1;
+                DecimalFormat df = new DecimalFormat("#.#");
+                df.setRoundingMode(RoundingMode.CEILING);
+                String station = df.format(sb_current);
+                fm_current.setText(station);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void next_station(View v)
+    {
+        fm_select.incrementProgressBy(1);
+    }
+
+    public void prev_station(View v)
+    {
+        fm_select.incrementProgressBy(-1);
+    }
+}
