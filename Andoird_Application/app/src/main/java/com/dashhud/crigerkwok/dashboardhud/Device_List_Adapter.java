7f9361b8d9ac8a0ce6de6689e52d4c27f45154a8
dcriger <dcriger@mail.csuchico.edu>
2018-02-01 07:20:37 -0800

    Bluetooth control page added, functionality for: - Bluetooth ON/OFF button - Device discoverable button - Discover unpaired devices button (adapter not working, list will not display) Next to come is pairing and data transfer for bluetooth, also currently no way to switch activities.

---
+++ Andoird_Application/app/src/main/java/com/dashhud/crigerkwok/dashboardhud/Device_List_Adapter.java
@@ -0,0 +1,50 @@
+package com.dashhud.crigerkwok.dashboardhud;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Device_List_Adapter extends AppCompatActivity {
+
+    private LayoutInflater li;
+    private ArrayList<BluetoothDevice> devices_array;
+    private int view_resource_id;
+
+    public Device_List_Adapter(Context context, int resource_id, ArrayList<BluetoothDevice> devices)
+    {
+        //super(context, resource_id, devices);
+        this.devices_array = devices;
+        li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view_resource_id = resource_id;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        convertView = li.inflate(view_resource_id, null);
+
+        BluetoothDevice device = devices_array.get(position);
+
+        if(device != null)
+        {
+            TextView device_name = convertView.findViewById(R.id.device_name);
+            TextView device_address = convertView.findViewById(R.id.device_address);
+
+            if(device_name != null)
+            {
+                device_name.setText(device.getName());
+            }
+            if(device_address != null)
+            {
+                device_address.setText(device.getAddress());
+            }
+        }
+        return convertView;
+    }
+}
