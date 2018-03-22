3c031c0e8354fe1086e22b4d7c3a9b72ffca4d69
dcriger <dcriger@mail.csuchico.edu>
2018-03-22 16:29:19 -0700

    Destination activity updated to display the devices actual location every time the activity is refreshed or launched. Some unused methods exist because "implements LocationListener" will throw a fit without them.

--- Andoird_Application/app/src/main/java/com/dashhud/crigerkwok/dashboardhud/Destination_Info.java
+++ Andoird_Application/app/src/main/java/com/dashhud/crigerkwok/dashboardhud/Destination_Info.java
@@ -1,8 +1,17 @@
 package com.dashhud.crigerkwok.dashboardhud;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.SyncStateContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +19,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Destination_Info extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class Destination_Info extends AppCompatActivity implements LocationListener{
 
     TextView current_address;
     TextView saved_address;
@@ -22,6 +35,12 @@ public class Destination_Info extends AppCompatActivity {
     EditText city;
 
     Button set_dest;
+
+    Geocoder gc;
+    List<Address> addresses;
+    LocationManager locationManager;
+    Double latitude;
+    Double longitude;
 
     SharedPreferences pref;
 
@@ -41,8 +60,12 @@ public class Destination_Info extends AppCompatActivity {
 
         set_dest = findViewById(R.id.set_destination_btn);
 
+        gc = new Geocoder(this, Locale.getDefault());
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         pref = getSharedPreferences("destination", Context.MODE_PRIVATE);
 
+        check_perms();
         update();
     }
 
@@ -55,7 +78,15 @@ public class Destination_Info extends AppCompatActivity {
     public void update()
     {
         //update phone's location
-
+        try {
+            addresses = gc.getFromLocation(latitude, longitude, 1);
+            String address_l = addresses.get(0).getAddressLine(0);
+            current_address.setText(address_l);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
         //update saved destination for current session
         saved_address.setText(pref.getString("saved_address", ""));
@@ -79,5 +110,39 @@ public class Destination_Info extends AppCompatActivity {
         //move back to controller
         Intent a = new Intent(Destination_Info.this, Controller.class);
         startActivity(a);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void check_perms()
+    {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
     }
 }
